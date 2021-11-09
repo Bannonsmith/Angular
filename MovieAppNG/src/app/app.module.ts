c374785e58ff5cd4c3143b420dfe76a5fb091198
Bannon smith <Bannonsmith13@gmail.com>
2021-11-09 03:57:32 -0600

    Generated basic layout

--- MovieAppNG/src/app/app.module.ts
+++ MovieAppNG/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { MoviePageComponent } from './components/movie-page/movie-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactusComponent,
+    MoviePageComponent
   ],
   imports: [
     BrowserModule,
