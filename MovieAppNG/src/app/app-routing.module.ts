3d1a695538b5e4853f8e0a9980a7066935b78c38
Bannon smith <Bannonsmith13@gmail.com>
2021-11-09 04:41:15 -0600

    added routes

--- MovieAppNG/src/app/app-routing.module.ts
+++ MovieAppNG/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { MoviePageComponent } from './components/movie-page/movie-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactusComponent},
+  {path: 'movies', component: MoviePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
